abd7a7e0c9785850a9be3a518a85e745d90842fd
Lucas Ribeiro Vidal <lribeirovidal@salesforce.com>
2022-06-27 11:46:22 -0700

    I am pinging and by god I am ponging

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const app = require("./app")
+import morgan from "morgan";
+import express from "express";
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`🚀 Server listening at http://localhost:${port}`)
-})
+	console.log(`🚀 Server listening at http://localhost:${port}`);
+});
+morgan("tiny");
+
+app.get("/", (req, res) => {
+	console.log(req);
+	res.send({ ping: "pong" });
+});
